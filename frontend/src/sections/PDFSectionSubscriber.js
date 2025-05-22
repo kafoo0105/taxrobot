c3f55ba0e89ba18136aecba2f6bcb649221014b5
kafoo0105 <kafoo0105@naver.com>
2025-05-22 20:02:37 +0900

    에러 메시지 관련 기능 구현

--- frontend/src/sections/PDFSectionSubscriber.js
+++ frontend/src/sections/PDFSectionSubscriber.js
@@ -1,5 +1,3 @@
-// src/sections/PDFSectionSubscriber.js
-
 import React from 'react';
 import styled from 'styled-components';
 
@@ -9,136 +7,146 @@ import styled from 'styled-components';
  * - 입력 조건 (readonly, editable 등)은 엑셀 기준에 따라 반영
  */
 const PDFSectionSubscriber = ({ formState, dispatch, invalidKeys = [] }) => {
-    const handleChange = (key, value) => {
-        dispatch({ type: "UPDATE_FIELD", key, value });
-    };
+  const handleChange = (key, value) => {
+    dispatch({ type: "UPDATE_FIELD", key, value });
+  };
 
-    const isInvalid = (key) => invalidKeys.includes(key);
+  const isInvalid = (key) => invalidKeys.includes(key);
 
-    return (
-        <Card>
-            <Header>
-                <Title>가입자 정보</Title>
-                <label>
-                    <input
-                        type="checkbox"
-                        checked={formState.taxInvoice}
-                        onChange={e => handleChange('taxInvoice', e.target.checked)}
-                    />
-                    세금계산서 발행
-                </label>
-            </Header>
+  return (
+    <Card>
+      <Header>
+        <Title>가입자 정보</Title>
+        <label>
+          <input
+            type="checkbox"
+            checked={formState.taxInvoice}
+            onChange={e => handleChange('taxInvoice', e.target.checked)}
+          />
+          세금계산서 발행
+        </label>
+      </Header>
 
-            <Grid>
-                <FormGroup>
-                    <Label>가입자명</Label>
-                    <ReadOnly>{formState.name}</ReadOnly>
-                </FormGroup>
+      <Grid>
+        <FormGroup>
+          <Label>가입자명</Label>
+          <ReadOnly>{formState.name}</ReadOnly>
+        </FormGroup>
 
-                <FormGroup>
-                    <Label>생년월일</Label>
-                    <ReadOnly>{formState.birth}</ReadOnly>
-                </FormGroup>
+        <FormGroup>
+          <Label>생년월일</Label>
+          <ReadOnly>{formState.birth}</ReadOnly>
+        </FormGroup>
 
-                <FormGroup>
-                    <Label>설치장치</Label>
-                    <ReadOnly>{formState.device}</ReadOnly>
-                </FormGroup>
+        <FormGroup>
+          <Label>설치장치</Label>
+          <ReadOnly>{formState.device}</ReadOnly>
+        </FormGroup>
 
-                <FormGroup>
-                    <Label>이동전화 연락처</Label>
-                    <ReadOnly>{formState.phone}</ReadOnly>
-                </FormGroup>
+        <FormGroup>
+          <Label>이동전화 연락처</Label>
+          <ReadOnly>{formState.phone}</ReadOnly>
+        </FormGroup>
 
-                <FormGroup>
-                    <Label>자동이체</Label>
-                    <Select
-                        value={formState.autoPaymentType}
-                        onChange={e => handleChange('autoPaymentType', e.target.value)}
-                    >
-                        <option value="">선택</option>
-                        <option value="계좌">계좌이체</option>
-                        <option value="카드">신용카드</option>
-                    </Select>
-                </FormGroup>
+        <FormGroup>
+          <Label>자동이체</Label>
+          <Select
+            value={formState.autoPaymentType}
+            onChange={e => handleChange('autoPaymentType', e.target.value)}
+            hasError={isInvalid('autoPaymentType')}
+          >
+            <option value="">선택</option>
+            <option value="계좌">계좌이체</option>
+            <option value="카드">신용카드</option>
+          </Select>
+          {isInvalid('autoPaymentType') && <ErrorText>필수 항목입니다.</ErrorText>}
+        </FormGroup>
 
-                <FormGroup>
-                    <Label>은행</Label>
-                    <Input
-                        value={formState.bank}
-                        onChange={e => handleChange('bank', e.target.value)}
-                        hasError={isInvalid('bank')}
-                    />
-                </FormGroup>
+        <FormGroup>
+          <Label>은행</Label>
+          <Input
+            value={formState.bank}
+            onChange={e => handleChange('bank', e.target.value)}
+            hasError={isInvalid('bank')}
+          />
+          {isInvalid('bank') && <ErrorText>필수 항목입니다.</ErrorText>}
+        </FormGroup>
 
-                <FormGroup>
-                    <Label>계좌번호</Label>
-                    <Input
-                        value={formState.account}
-                        onChange={e => handleChange('account', e.target.value)}
-                        hasError={isInvalid('account')}
-                    />
-                </FormGroup>
+        <FormGroup>
+          <Label>계좌번호</Label>
+          <Input
+            value={formState.account}
+            onChange={e => handleChange('account', e.target.value)}
+            hasError={isInvalid('account')}
+          />
+          {isInvalid('account') && <ErrorText>필수 항목입니다.</ErrorText>}
+        </FormGroup>
 
-                <FormGroup>
-                    <Label>예금주</Label>
-                    <Input
-                        value={formState.holder}
-                        onChange={e => handleChange('holder', e.target.value)}
-                        hasError={isInvalid('holder')}
-                    />
-                </FormGroup>
+        <FormGroup>
+          <Label>예금주</Label>
+          <Input
+            value={formState.holder}
+            onChange={e => handleChange('holder', e.target.value)}
+            hasError={isInvalid('holder')}
+          />
+          {isInvalid('holder') && <ErrorText>필수 항목입니다.</ErrorText>}
+        </FormGroup>
 
-                <FormGroup>
-                    <Label>카드번호</Label>
-                    <Input
-                        value={formState.cardNumber}
-                        onChange={e => handleChange('cardNumber', e.target.value)}
-                        hasError={isInvalid('cardNumber')}
-                    />
-                </FormGroup>
+        <FormGroup>
+          <Label>카드번호</Label>
+          <Input
+            value={formState.cardNumber}
+            onChange={e => handleChange('cardNumber', e.target.value)}
+            hasError={isInvalid('cardNumber')}
+          />
+          {isInvalid('cardNumber') && <ErrorText>필수 항목입니다.</ErrorText>}
+        </FormGroup>
 
-                <FormGroup>
-                    <Label>카드유효기간</Label>
-                    <Input
-                        value={formState.cardExpire}
-                        onChange={e => handleChange('cardExpire', e.target.value)}
-                        hasError={isInvalid('cardExpire')}
-                    />
-                </FormGroup>
+        <FormGroup>
+          <Label>카드유효기간</Label>
+          <Input
+            value={formState.cardExpire}
+            onChange={e => handleChange('cardExpire', e.target.value)}
+            hasError={isInvalid('cardExpire')}
+          />
+          {isInvalid('cardExpire') && <ErrorText>필수 항목입니다.</ErrorText>}
+        </FormGroup>
 
-                <FormGroup>
-                    <Label>카드주</Label>
-                    <Input
-                        value={formState.cardHolder}
-                        onChange={e => handleChange('cardHolder', e.target.value)}
-                        hasError={isInvalid('cardHolder')}
-                    />
-                </FormGroup>
+        <FormGroup>
+          <Label>카드주</Label>
+          <Input
+            value={formState.cardHolder}
+            onChange={e => handleChange('cardHolder', e.target.value)}
+            hasError={isInvalid('cardHolder')}
+          />
+          {isInvalid('cardHolder') && <ErrorText>필수 항목입니다.</ErrorText>}
+        </FormGroup>
 
-                <FormGroup>
-                    <Label>요금안내 이메일</Label>
-                    <Input
-                        value={formState.email}
-                        onChange={e => handleChange('email', e.target.value)}
-                        hasError={isInvalid('email')}
-                    />
-                </FormGroup>
-            </Grid>
+        <FormGroup>
+          <Label>요금안내 이메일</Label>
+          <Input
+            value={formState.email}
+            onChange={e => handleChange('email', e.target.value)}
+            hasError={isInvalid('email')}
+          />
+          {isInvalid('email') && <ErrorText>필수 항목입니다.</ErrorText>}
+        </FormGroup>
+      </Grid>
 
-            <Footer>
-                <label>
-                    <input
-                        type="checkbox"
-                        checked={formState.agreePayment}
-                        onChange={e => handleChange('agreePayment', e.target.checked)}
-                    />
-                    예금자(카드주) 동의
-                </label>
-                <SignatureBox>서명 / 인</SignatureBox>
-            </Footer>
-        </Card>
-    );
+      <Footer>
+        <label>
+          <input
+            type="checkbox"
+            checked={formState.agreePayment}
+            onChange={e => handleChange('agreePayment', e.target.checked)}
+          />
+          예금자(카드주) 동의
+          {isInvalid('agreePayment') && <ErrorText>필수 항목입니다.</ErrorText>}
+        </label>
+        <SignatureBox>서명 / 인</SignatureBox>
+      </Footer>
+    </Card>
+  );
 };
 
 export default PDFSectionSubscriber;
@@ -212,10 +220,11 @@ const Input = styled.input`
 
 const Select = styled.select`
   padding: 12px;
-  border: 1px solid #ccc;
+  border: 1px solid ${props => props.hasError ? '#ff4d4f' : '#ccc'};
   border-radius: 6px;
   font-size: 14px;
 `;
+
 
 const Footer = styled.div`
   margin-top: 32px;
@@ -241,6 +250,8 @@ const SignatureBox = styled.div`
   background: #fafafa;
 `;
 
-const EditableInput = styled(Input)`
-  background: #fff;
+const ErrorText = styled.div`
+  color: #ff4d4f;
+  font-size: 12px;
+  margin-top: 4px;
 `;
--- frontend/src/utils/validateForm.js
+++ frontend/src/utils/validateForm.js
@@ -1,8 +1,7 @@
-// src/utils/validateForm.js
-
 /**
  * 필수 항목이 누락된 키 목록을 반환하는 함수
  * 이후 항목별 required 설정도 가능하지만 현재는 하드코딩
+ * formState는 매개변수, TermsPage.js의 formState 상태를 넘겨받을 부분
  */
 export const validateForm = (formState) => {
   const requiredKeys = [

26dde7855737e3c913b4c6a18953a22ddce612bd
kafoo0105 <kafoo0105@naver.com>
2025-05-23 17:08:37 +0900

    입력란 오류 수정

--- frontend/src/utils/validateForm.js
+++ frontend/src/utils/validateForm.js
@@ -4,23 +4,29 @@
  * formState는 매개변수, TermsPage.js의 formState 상태를 넘겨받을 부분
  */
 export const validateForm = (formState) => {
-  const requiredKeys = [
+  const { autoPaymentType } = formState;
+
+  // 공통 필수 항목
+  let requiredKeys = [
     'name',
     'birth',
     'phone',
     'address',
     'autoPaymentType',
-    'bank',
-    'account',
-    'holder',
-    'cardNumber',
-    'cardExpire',
-    'cardHolder',
     'email',
     'agreePayment'
   ];
 
-  // formState에서 필수 입력값이 빠진 항목만 찾아냄
+  // 조건부 필수 항목
+  if (autoPaymentType === '계좌') {
+    requiredKeys.push('bank', 'account', 'holder');
+  }
+
+  if (autoPaymentType === '카드') {
+    requiredKeys.push('cardNumber', 'cardExpire', 'cardHolder');
+  }
+
+  // 누락된 항목 필터링
   const emptyFields = requiredKeys.filter((key) => {
     const value = formState[key];
     if (typeof value === 'boolean') return !value;
@@ -30,3 +36,4 @@ export const validateForm = (formState) => {
 
   return emptyFields;
 };
+
